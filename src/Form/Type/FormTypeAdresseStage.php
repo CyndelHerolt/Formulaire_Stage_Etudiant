e24f1f3de9b0f084abdcb0a6d6435382882b1a00
CyndelHerolt <herolt.cyndel@gmail.com>
2022-10-29 12:14:35 +0200

    new commit

--- src/Form/Type/FormTypeAdresseStage.php
+++ src/Form/Type/FormTypeAdresseStage.php
@@ -5,16 +5,11 @@ namespace App\Form\Type;
 
 use App\Entity\StageEtudiant;
 use App\Form\AdresseType;
-use App\Form\EntrepriseType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
 
 Class FormTypeAdresseStage extends AbstractType
 {
